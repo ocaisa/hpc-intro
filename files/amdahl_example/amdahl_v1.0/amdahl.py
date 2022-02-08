da259f728be4e9285fcab4aa2f221fefa9c85ee6
Trevor Keller <trevor.keller@nist.gov>
2022-02-08 17:27:34 -0500

    docs: theory & worker process output indents

--- files/amdahl_example/amdahl_v1.0/amdahl.py
+++ files/amdahl_example/amdahl_v1.0/amdahl.py
@@ -1,13 +1,60 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
-Amdahls law illustrator (with fake work)
+Amdahl's law illustrator (with fake work to minimize actual CPU load)
+
+Amdahl's law posits that some unit of work comprises a proportion *p* that
+benefits from parallel resources, and a proportion *s* that is constrained to
+execute in serial, only. The theoretical maximum speedup achievable for such a
+workload is
+
+           1
+    S = -------
+        s + p/N
+
+where *S* is the speedup relative to performing all of the work in serial and
+*N* is the number of parallel workers. A plot of *S* vs. *N* ought to look like
+this, for *p*=0.8:
+
+  5┬─────────────────────────────────────·──────────────────┐
+   │                                   ·                    │
+   │                                 ·                      │
+   │                               ·                        │
+  4┤                             ·                          │
+   │                           ·                            │
+S  │                         ·                              *
+p  │                       ·                   *      *     │
+e  │                     ·               *                  │
+e 3┤                   ·           *                        │
+d  │                 ·      *                               │
+u  │               ·  *                                     │
+p  │             ·                                          │
+   │           ·*                                           |
+  2┤         ·                                              │
+   │     * ·                                                │
+   │     ·                                                  │
+   │   ·                                                    │
+   │ ·                                                      │
+  1*─────┬──────┬─────┬─────┬──────┬─────┬─────┬──────┬─────┤
+   1     2      3     4     5      6     7     8      9     10
+                             Workers
+
+"Ideal scaling" (*p*=1) would be the line *y* = *x* (or *S* = *N*), represented
+by the dotted line.
+
+This graph shows there is a speed limit for every workload, and diminishing
+returns on throwing more parallel processors at a problem. It is worth running
+a "scaling study" to assess how far away that speed limit might be for the
+given task.
 """
 
-from mpi4py import MPI
-import sys
 import time
+# Start a clock to measure total time
+start = time.time()
+
+import sys
 import argparse
 
+from mpi4py import MPI
 
 def do_work(work_time=30, parallel_proportion=0.8, comm=MPI.COMM_WORLD):
     # How many MPI ranks (cores) are we?
@@ -18,24 +65,28 @@ def do_work(work_time=30, parallel_proportion=0.8, comm=MPI.COMM_WORLD):
     name = MPI.Get_processor_name()
 
     if rank == 0:
-        # use Amdahls law to calculate the expected speedup for a given workload
-        amdahl_speed_up = 1.0 / (
-            (1.0 - parallel_proportion) + parallel_proportion / size
-        )
-
         # Set the sleep times (which are used to fake the amount of work)
         serial_sleep_time = float(work_time) * (1.0 - parallel_proportion)
         parallel_sleep_time = (float(work_time) * parallel_proportion) / size
 
+        # Use Amdahl's law to calculate the expected speedup for this workload
+        amdahl_speed_up = 1.0 / (
+            (1.0 - parallel_proportion) + parallel_proportion / size
+        )
+
+        suffix = "" if size == 1 else "s"
+
         sys.stdout.write(
-            "Processors will do %s seconds of 'work', which should take %s seconds "
-            "on %s cores with %s parallel proportion of the workload.\n"
-            % (work_time, work_time / amdahl_speed_up, size, parallel_proportion)
+            "Doing %f seconds of 'work' on %s processor%s,\n"
+            " which should take %f seconds with %f parallel"
+            " proportion of the workload.\n\n"
+            % (work_time, size, suffix, work_time / amdahl_speed_up, parallel_proportion)
         )
 
         sys.stdout.write(
-            "Hello, World! I am process %d of %d on %s and I will do all the serial "
-            "'work' for %s seconds.\n" % (rank, size, name, serial_sleep_time)
+            "  Hello, World! I am process %d of %d on %s."
+            " I will do all the serial 'work' for"
+            " %f seconds.\n" % (rank, size, name, serial_sleep_time)
         )
         time.sleep(serial_sleep_time)
     else:
@@ -48,8 +99,8 @@ def do_work(work_time=30, parallel_proportion=0.8, comm=MPI.COMM_WORLD):
 
     # This is where everyone pretends to do work (while really we are just sleeping)
     sys.stdout.write(
-        "Hello, World! I am process %d of %d on %s and I will do parallel 'work' for "
-        "%s seconds.\n" % (rank, size, name, parallel_sleep_time)
+        "  Hello, World! I am process %d of %d on %s. I will do parallel 'work' for "
+        "%f seconds.\n" % (rank, size, name, parallel_sleep_time)
     )
     time.sleep(parallel_sleep_time)
 
@@ -57,8 +108,6 @@ def do_work(work_time=30, parallel_proportion=0.8, comm=MPI.COMM_WORLD):
 # Only the root process handles the command line arguments
 rank = MPI.COMM_WORLD.Get_rank()
 if rank == 0:
-    # Start a clock to measure total time
-    start = time.time()
     # Initialize our argument parser
     parser = argparse.ArgumentParser(prog="amdahl")
 
@@ -98,7 +147,7 @@ if rank == 0:
     do_work(work_time=args.work_seconds, parallel_proportion=args.parallel_proportion)
     end = time.time()
     sys.stdout.write(
-        "Total execution time (according to rank 0): %s seconds\n" % (end - start)
+        "\nTotal execution time (according to rank 0): %f seconds\n" % (end - start)
     )
 else:
     do_work()
